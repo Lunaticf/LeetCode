c27e586a96bf0cc38e6965d358f745db9fa0e6e5
lunaticf <724922441@qq.com>
2019-03-29 19:41:00 +0800

    Finish 724

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # [LeetCode](https://leetcode.com/problemset/algorithms/) ![Language](https://img.shields.io/badge/language-%20Java%20-green.svg) [![License](https://img.shields.io/badge/license-MIT-blue.svg)](./LICENSE.md) 
 
 Java solutions of Leetcode
+## Array
+
+| # | Title | Solution | Difficulty |
+|---| ----- | -------- | ---------- |
+|724|[Find pivot index](https://leetcode.com/problems/find-pivot-index/) | [code](./array/findPivotIndex/FindPivotIndex.java)/[note](./array/findPivotIndex/FindPivotIndex.md) | Easy |
---
+++ array/findPivotIndex/FindPivotIndex.java
@@ -0,0 +1,40 @@
+package LeetCode.array.findPivotIndex;
+
+public class FindPivotIndex {
+
+    // brute force
+    public int pivotIndex1(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int leftSum = 0;
+            for (int p = 0; p < i; p++) {
+                leftSum += nums[p];
+            }
+            int rightSum = 0;
+            for (int q = i + 1; q < nums.length; q++) {
+                rightSum += nums[q];
+            }
+            if (leftSum == rightSum) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // optimization
+    public int pivotIndex(int[] nums) {
+        int sum = 0, leftSum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (leftSum == sum - nums[i] - leftSum) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+
+}
