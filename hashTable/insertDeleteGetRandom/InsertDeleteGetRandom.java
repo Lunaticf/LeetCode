4e1f555e7ec9f59c1b20a8f91412377bf8945931
lunaticf <724922441@qq.com>
2019-06-09 18:12:58 +0800

    380

---
+++ hashTable/insertDeleteGetRandom/InsertDeleteGetRandom.java
@@ -0,0 +1,65 @@
+package LeetCode.hashTable.insertDeleteGetRandom;
+
+import java.util.*;
+
+public class InsertDeleteGetRandom {
+
+    /**
+     * Your RandomizedSet object will be instantiated and called as such:
+     * RandomizedSet obj = new RandomizedSet();
+     * boolean param_1 = obj.insert(val);
+     * boolean param_2 = obj.remove(val);
+     * int param_3 = obj.getRandom();
+     */
+
+    class RandomizedSet {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        Random random = new Random();
+
+
+        /** Initialize your data structure here. */
+        public RandomizedSet() {
+
+        }
+
+        /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+        public boolean insert(int val) {
+            if (hashMap.containsKey(val)) {
+                return false;
+            } else {
+                // 如果没有 插入
+                hashMap.put(val, list.size());
+                list.add(val);
+                return true;
+            }
+        }
+
+        /** Removes a value from the set. Returns true if the set contained the specified element. */
+        public boolean remove(int val) {
+            if (hashMap.containsKey(val)) {
+                int i = hashMap.get(val);
+
+                // 如果不是最后一个元素
+                if (i < list.size() - 1) {
+                    int lastOne = list.get(list.size() - 1);
+                    list.set(i, lastOne);
+                    hashMap.put(lastOne, i);
+                }
+
+                list.remove(list.size() - 1);
+                hashMap.remove(val);
+
+                return true;
+            }
+            return false;
+        }
+
+        /** Get a random element from the set. */
+        public int getRandom() {
+            int num = random.nextInt(list.size());
+            return list.get(num);
+        }
+    }
+
+}
---
+++ hashTable/insertDeleteGetRandom/InsertDeleteGetRandom.md
@@ -0,0 +1,7 @@
+## 380. Insert Delete GetRandom O(1)
+设计一个数据结构
+各种操作O(1)的复杂度
+就是插入删除，和随机得到一个元素
+比HashSet多了一个功能随即得到一个元素。
+可以用HashMap记录元素对应的在ArrayList中的索引
+
