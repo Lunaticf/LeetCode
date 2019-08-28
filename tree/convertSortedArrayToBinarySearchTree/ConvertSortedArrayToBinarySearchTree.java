bedeb6c7d9ebcf0b0975afe7fe47eb19c9a2f7f0
center-liu <liu.chaofan@foxmail.com>
2019-08-28 16:39:41 +0800

    108

---
+++ tree/convertSortedArrayToBinarySearchTree/ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,26 @@
+package LeetCode.tree.convertSortedArrayToBinarySearchTree;
+
+
+import LeetCode.tree.TreeNode;
+
+public class ConvertSortedArrayToBinarySearchTree {
+
+    // 将一个递增数组变成bst, 并返回树根
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return recur(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode recur(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+
+        int mid = (right + left) / 2;
+        TreeNode t = new TreeNode(nums[mid]);
+        t.left = recur(nums, left, mid - 1);
+        t.right = recur(nums, mid + 1, right);
+        return t;
+    }
+
+
+}
---
+++ tree/convertSortedArrayToBinarySearchTree/ConvertSortedArrayToBinarySearchTree.md
@@ -0,0 +1,4 @@
+## 108. Convert Sorted Array to Binary Search Tree
+
+将一个递增的数组转化成平衡的二叉搜索树
+应该是可以从中间不停的剖开分开左右树
