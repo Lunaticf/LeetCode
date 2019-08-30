46eb9c3397aa54010ddf50b753f228aa61605c17
center-liu <liu.chaofan@foxmail.com>
2019-08-30 17:15:01 +0800

    valid palindrome

---
+++ string/validPalindrome/ValidPalindrome.java
@@ -0,0 +1,27 @@
+package LeetCode.string.validPalindrome;
+
+public class ValidPalindrome {
+    public static void main(String[] args) {
+        String s = ".,";
+        new ValidPalindrome().isPalindrome(s);
+    }
+
+    public boolean isPalindrome(String s) {
+        if (s.equals("")) return true;
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            while (i < s.length() && !Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+            }
+            while (j >= 0 && !Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            }
+            if(i > j) {return true;}
+            if (Character.toLowerCase(s.charAt(i++)) != Character.toLowerCase(s.charAt(j--))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
