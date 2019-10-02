829e24a84b09ae0d336b0fcaa834ed6c17d2bfc5
Lunaticf <724922441@qq.com>
2019-10-02 11:57:32 +0800

    383

---
+++ string/ransomNote/RansomNote.java
@@ -0,0 +1,16 @@
+package leetCode.string.ransomNote;
+
+public class RansomNote {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] counter = new int[26];
+        for (int i = 0; i < magazine.length(); i++) {
+            counter[magazine.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < ransomNote.length(); i++) {
+            if (--counter[ransomNote.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
