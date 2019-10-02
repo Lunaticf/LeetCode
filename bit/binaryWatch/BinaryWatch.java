91c67245e31fca12d57c7101d2985d86e2b0685a
Lunaticf <724922441@qq.com>
2019-10-02 14:41:01 +0800

    401 half

---
+++ bit/binaryWatch/BinaryWatch.java
@@ -0,0 +1,18 @@
+package leetCode.bit.binaryWatch;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryWatch {
+    public List<String> readBinaryWatch(int num) {
+        List<String> res = new ArrayList<>();
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 60; j++) {
+                if (Integer.bitCount(i) + Integer.bitCount(j) == num) {
+                    res.add(String.format("%d:%02d", i, j));
+                }
+            }
+        }
+        return res;
+    }
+}
