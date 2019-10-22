566162ce1d7ed15565d2816208642c3ed45bd174
center-liu <liu.chaofan@foxmail.com>
2019-10-22 10:25:48 +0800

    709

---
+++ string/toLowerCase/ToLowerCase.java
@@ -0,0 +1,22 @@
+package leetCode.string.toLowerCase;
+
+/**
+ * @Author Lunaticf
+ * @Date 2019/10/22
+ */
+public class ToLowerCase {
+    public String toLowerCase1(String str) {
+        return str.toLowerCase();
+    }
+
+    public String toLowerCase(String str) {
+        char[] ca = str.toCharArray();
+        for (int i = 0;i < ca.length; i++) {
+            if (ca[i] <= 'Z' && ca[i] >= 'A') {
+                ca[i] += 32;
+//                ca[i] = (char) (ca[i] - 'A' + 'a');
+            }
+        }
+        return String.valueOf(ca);
+    }
+}
