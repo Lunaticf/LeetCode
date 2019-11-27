e5ecc1d873680f1b7431aa4c0e4fae627e1a4b9a
Lunaticf <724922441@qq.com>
2019-11-27 17:43:26 +0800

    dwqdw

---
+++ dynamicProgramming/UniquePath.java
@@ -0,0 +1,19 @@
+package leetCode.dynamicProgramming;
+
+public class UniquePath {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i - 1 < 0 || j - 1 < 0) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+
+        return  dp[m - 1][n - 1];
+    }
+}
